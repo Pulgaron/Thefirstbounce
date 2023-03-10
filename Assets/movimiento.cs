40cf192eef2cf11f11307b742ef80ffb20847af7
Daniell58 <andres.quijano58@gmail.com>
2023-03-10 17:46:16 -0600

    añadido holamundo

--- Assets/movimiento.cs
+++ Assets/movimiento.cs
@@ -7,7 +7,7 @@ public class movimiento : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("holamundo");
     }
 
     // Update is called once per frame
